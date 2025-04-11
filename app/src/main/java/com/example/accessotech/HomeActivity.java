d8b5ccb7f6685deee08df8d75d918cabf46bef0d
Mays Al-Reem <hroubmays@gmail.com>
2025-04-11 21:46:43 +0200

    feat: add intent to open home screen

--- app/src/main/java/com/example/accessotech/HomeActivity.java
+++ app/src/main/java/com/example/accessotech/HomeActivity.java
@@ -1,8 +1,6 @@
 package com.example.accessotech;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-            startActivity(intent);
-            finish();
-        }, 5000);
     }
 }
