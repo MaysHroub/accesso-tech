dd48fd67da8c360f56c9cb2ca8d1bb6cec01decc
Mays Al-Reem <hroubmays@gmail.com>
2025-04-15 21:41:50 +0300

    refactor: move activities to activity package

--- app/src/main/java/com/example/accessotech/activity/ItemDetailsActivity.java
+++ app/src/main/java/com/example/accessotech/activity/ItemDetailsActivity.java
@@ -1,4 +1,4 @@
-package com.example.accessotech;
+package com.example.accessotech.activity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CartActivity extends AppCompatActivity {
+import com.example.accessotech.R;
+
+public class ItemDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_item_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/accessotech/activity/SignUpActivity.java
+++ app/src/main/java/com/example/accessotech/activity/SignUpActivity.java
@@ -1,4 +1,4 @@
-package com.example.accessotech;
+package com.example.accessotech.activity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UserProfileActivity extends AppCompatActivity {
+import com.example.accessotech.R;
+
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_profile);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
