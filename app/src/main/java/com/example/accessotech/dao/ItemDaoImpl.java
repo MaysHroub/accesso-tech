6a2dca1c62b4724d78377ba75d9d55f0f2694fe5
Mays Al-Reem <hroubmays@gmail.com>
2025-04-16 14:50:15 +0300

    feat: implement ItemDao interface

---
+++ app/src/main/java/com/example/accessotech/dao/ItemDaoImpl.java
@@ -0,0 +1,73 @@
+package com.example.accessotech.dao;
+
+import com.example.accessotech.model.Item;
+import com.example.accessotech.util.DataLoader;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ItemDaoImpl implements ItemDao {
+
+    private final List<Item> items;
+    private final Set<String> categories, manufacturers;
+
+    public ItemDaoImpl() {
+        items = new ArrayList<>();
+        categories = new HashSet<>();
+        manufacturers = new HashSet<>();
+        DataLoader dataLoader = new DataLoader(this);
+        dataLoader.loadData();
+    }
+
+    @Override
+    public void saveAllItems(List<Item> items) {
+        this.items.addAll(items);
+    }
+
+    @Override
+    public void saveAllCategories(Set<String> categories) {
+        this.categories.addAll(categories);
+    }
+
+    @Override
+    public void saveAllManufacturers(Set<String> manufacturers) {
+        this.manufacturers.addAll(manufacturers);
+    }
+
+    @Override
+    public List<Item> findAllItems() {
+        return items;
+    }
+
+    @Override
+    public Item findItemById(int id) {
+        Item item = new Item();
+        item.setId(id);
+        int idx;
+        return ((idx = items.indexOf(item)) != -1) ? items.get(idx) : null;
+    }
+
+    @Override
+    public Set<String> findAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public Set<String> findAllManufacturers() {
+        return manufacturers;
+    }
+
+    @Override
+    public void deleteAllItems() {
+        items.clear();
+    }
+
+    @Override
+    public void deleteItemById(int id) {
+        Item item = new Item();
+        item.setId(id);
+        items.remove(item);
+    }
+}
