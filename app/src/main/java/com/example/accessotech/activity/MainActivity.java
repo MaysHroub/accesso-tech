8196834e11138df7d6dbf60d02735a4babbb0412
Mays Al-Reem <hroubmays@gmail.com>
2025-04-16 17:42:18 +0300

    Âfeat: check there's account and launch right activity

--- app/src/main/java/com/example/accessotech/activity/MainActivity.java
+++ app/src/main/java/com/example/accessotech/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.accessotech.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,8 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.accessotech.R;
+import com.example.accessotech.util.PrefsKeys;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        prefs = getSharedPreferences(PrefsKeys.MY_SHARED_PREFS.getKey(), Context.MODE_PRIVATE);
     }
 
     @Override
@@ -31,7 +37,12 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Handler handler = new Handler();
         handler.postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            boolean hasAccount = prefs.getBoolean(PrefsKeys.HAS_ACCOUNT.getKey(), false);
+            Intent intent;
+            if (hasAccount)
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+            else
+                intent = new Intent(MainActivity.this, SignUpActivity.class);
             startActivity(intent);
             finish();
         }, 5000);
