6bf3ba6b3694b26f1e76fb584541e3f1bbf4ed6b
Mays Al-Reem <hroubmays@gmail.com>
2025-04-16 21:30:05 +0300

    feat: implement login functionality

--- app/src/main/java/com/example/accessotech/activity/LoginActivity.java
+++ app/src/main/java/com/example/accessotech/activity/LoginActivity.java
@@ -1,6 +1,14 @@
 package com.example.accessotech.activity;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +17,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.accessotech.R;
+import com.example.accessotech.util.InputValidator;
+import com.example.accessotech.util.PrefsKeys;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtTxtUsername, edtTxtPassword;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +35,41 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        prefs = getSharedPreferences(PrefsKeys.ACCOUNT_INFO_SHARED_PREF.getKey(), Context.MODE_PRIVATE);
+        setUpViews();
+    }
+
+    private void setUpViews() {
+        edtTxtUsername = findViewById(R.id.edtTxtUsernameLogin);
+        edtTxtPassword = findViewById(R.id.edtTxtPasswordLogin);
+    }
+
+    public void login(View view) {
+        String toastMessage;
+        if (!InputValidator.validateEditTexts(edtTxtUsername, edtTxtPassword))
+            toastMessage = "Some fields are still empty";
+        else {
+            boolean isUserAuthenticated = authenticateUser();
+            if (isUserAuthenticated) {
+                toastMessage = "Welcome " + edtTxtUsername.getText().toString();
+                launchHomeActivity();
+            }
+            else
+                toastMessage = "Wrong username or password";
+        }
+        Toast.makeText(this, toastMessage, LENGTH_SHORT).show();
+    }
+
+    private boolean authenticateUser() {
+        String username = prefs.getString(PrefsKeys.USERNAME.getKey(), "null");
+        String password = prefs.getString(PrefsKeys.PASSWORD.getKey(), "null");
+        return username.equals(edtTxtUsername.getText().toString()) &&
+                password.equals(edtTxtPassword.getText().toString());
+    }
+
+    private void launchHomeActivity() {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
