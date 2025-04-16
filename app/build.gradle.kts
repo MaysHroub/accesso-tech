b6faa3217d37f7937890877f479d754e0de071fa
Mays Al-Reem <hroubmays@gmail.com>
2025-04-16 18:42:17 +0300

    build: add lombok dependency to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,8 @@ android {
 }
 
 dependencies {
-
+    annotationProcessor(libs.lombok.v11838)
+    annotationProcessor(libs.lombok.v11838)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
