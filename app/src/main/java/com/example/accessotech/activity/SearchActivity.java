af24c7ab4f7b967c503b5f26c9ffc685bfed9a33
Mays Al-Reem <hroubmays@gmail.com>
2025-04-18 12:01:37 +0300

    feat: setup views and load data to spinners

--- app/src/main/java/com/example/accessotech/activity/SearchActivity.java
+++ app/src/main/java/com/example/accessotech/activity/SearchActivity.java
@@ -1,16 +1,31 @@
 package com.example.accessotech.activity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.accessotech.R;
+import com.example.accessotech.dao.ItemDao;
+import com.example.accessotech.dao.ItemDaoImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private EditText edtTxtSearch, edtTxtFrom, edtTxtTo;
+    private Spinner spnrCategory, spnrManufacturer, spnrRating;
+    private RecyclerView recyclerViewFilteredItems;
+    private ItemDao itemDao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +37,52 @@ public class SearchActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        itemDao = new ItemDaoImpl();
+        setUpViews();
+        fillViewsWithData();
     }
+
+    private void setUpViews() {
+        edtTxtSearch = findViewById(R.id.edtTxtSearch);
+        edtTxtFrom = findViewById(R.id.edtTxtFromPrice);
+        edtTxtTo = findViewById(R.id.edtTxtToPrice);
+        spnrCategory = findViewById(R.id.spinnerCategory);
+        spnrManufacturer = findViewById(R.id.spinnerCompany);
+        spnrRating = findViewById(R.id.spinnerRating);
+        recyclerViewFilteredItems = findViewById(R.id.recyclerViewFilteredItems);
+    }
+
+    private void fillViewsWithData() {
+        fillSpinnersWithData();
+        fillRecyclerViewWithData();
+    }
+
+    private void fillSpinnersWithData() {
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                itemDao.findAllCategories()
+        );
+        spnrCategory.setAdapter(categoryAdapter);
+
+        ArrayAdapter<String> manufacturerAdapter = new ArrayAdapter<>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                itemDao.findAllManufacturers()
+        );
+        spnrManufacturer.setAdapter(manufacturerAdapter);
+
+        Integer[] ratings = {1, 2, 3, 4, 5};
+        ArrayAdapter<Integer> ratingAdapter = new ArrayAdapter<>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                ratings
+        );
+        spnrRating.setAdapter(ratingAdapter);
+    }
+
+    private void fillRecyclerViewWithData() {
+
+    }
+
 }
