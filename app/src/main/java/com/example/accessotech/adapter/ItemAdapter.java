6220ec0b4d9021147afdc3792a2e68fb46c04045
Mays Al-Reem <hroubmays@gmail.com>
2025-04-18 12:05:43 +0300

    refactor: extract ItemViewHolder to a separate class

--- app/src/main/java/com/example/accessotech/adapter/ItemAdapter.java
+++ app/src/main/java/com/example/accessotech/adapter/ItemAdapter.java
@@ -3,24 +3,19 @@ package com.example.accessotech.adapter;
 import static android.view.View.GONE;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.accessotech.R;
-import com.example.accessotech.activity.ItemDetailsActivity;
 import com.example.accessotech.model.Item;
-import com.google.gson.Gson;
 
 import java.util.List;
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
     private List<Item> items;
     private Context context;
@@ -48,6 +43,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
             holder.txtViewDiscount.setText(String.format("%d%% OFF", item.getDiscount()));
         else
             holder.txtViewDiscount.setVisibility(GONE);
+
         // TODO: Fix the image path and replace it with resource id
         // holder.imgItem.setImageURI(Uri.parse(item.getImgUrl()));
 
@@ -57,35 +53,6 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
     @Override
     public int getItemCount() {
         return items.size();
-    }
-
-    static class ItemViewHolder extends RecyclerView.ViewHolder {
-        TextView txtViewName, txtViewPrice, txtViewRating, txtViewQuantityInStock, txtViewDiscount;
-        ImageView imgItem;
-        private View itemView;
-
-        public ItemViewHolder(@NonNull View itemView) {
-            super(itemView);
-            this.itemView = itemView;
-            txtViewName = itemView.findViewById(R.id.txtViewItemName);
-            txtViewPrice = itemView.findViewById(R.id.txtViewItemPrice);
-            txtViewRating = itemView.findViewById(R.id.txtViewItemRating);
-            txtViewQuantityInStock = itemView.findViewById(R.id.txtViewItemQuantityInStock);
-            txtViewDiscount = itemView.findViewById(R.id.txtViewItemDiscount);
-            imgItem = itemView.findViewById(R.id.imgItem);
-        }
-
-        public void addClickListener(Context context, Item item) {
-            itemView.setOnClickListener(v -> {
-                Intent intent = new Intent(itemView.getContext(), ItemDetailsActivity.class);
-                Gson gson = new Gson();
-                String json = gson.toJson(item);
-                intent.putExtra("activity", context.getClass().getName());
-                intent.putExtra("item", json);
-                context.startActivity(intent);
-            });
-        }
-
     }
 
 }
---
+++ app/src/main/java/com/example/accessotech/adapter/ItemViewHolder.java
@@ -0,0 +1,45 @@
+package com.example.accessotech.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.accessotech.R;
+import com.example.accessotech.activity.ItemDetailsActivity;
+import com.example.accessotech.model.Item;
+import com.google.gson.Gson;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+    TextView txtViewName, txtViewPrice, txtViewRating, txtViewQuantityInStock, txtViewDiscount;
+    ImageView imgItem;
+    private final View itemView;
+
+    public ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.itemView = itemView;
+        txtViewName = itemView.findViewById(R.id.txtViewItemName);
+        txtViewPrice = itemView.findViewById(R.id.txtViewItemPrice);
+        txtViewRating = itemView.findViewById(R.id.txtViewItemRating);
+        txtViewQuantityInStock = itemView.findViewById(R.id.txtViewItemQuantityInStock);
+        txtViewDiscount = itemView.findViewById(R.id.txtViewItemDiscount);
+        imgItem = itemView.findViewById(R.id.imgItem);
+    }
+
+    public void addClickListener(Context context, Item item) {
+        itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(itemView.getContext(), ItemDetailsActivity.class);
+            Gson gson = new Gson();
+            String json = gson.toJson(item);
+            intent.putExtra("activity", context.getClass().getName());
+            intent.putExtra("item", json);
+            context.startActivity(intent);
+        });
+    }
+
+}
