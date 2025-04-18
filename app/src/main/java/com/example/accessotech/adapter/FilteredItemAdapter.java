87e0279f1c03614ee8f5b3c8d8cd4deec010cc17
Mays Al-Reem <hroubmays@gmail.com>
2025-04-18 16:33:05 +0300

    feat: separate price to fromPrice and toPrice

--- app/src/main/java/com/example/accessotech/adapter/FilteredItemAdapter.java
+++ app/src/main/java/com/example/accessotech/adapter/FilteredItemAdapter.java
@@ -5,11 +5,13 @@ import static android.view.View.GONE;
 import static com.example.accessotech.activity.SearchActivity.CATEGORY_OPTION;
 import static com.example.accessotech.activity.SearchActivity.MANUFACTURER_OPTION;
 import static com.example.accessotech.adapter.ItemFilter.CATEGORY;
+import static com.example.accessotech.adapter.ItemFilter.FROM_PRICE;
 import static com.example.accessotech.adapter.ItemFilter.MANUFACTURER;
-import static com.example.accessotech.adapter.ItemFilter.PRICE;
 import static com.example.accessotech.adapter.ItemFilter.RATING;
+import static com.example.accessotech.adapter.ItemFilter.TO_PRICE;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -67,10 +69,11 @@ public class FilteredItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
     public void filter(String searchText, ItemFilter itemFilter) {
         filteredItems.clear();
+
         String category = itemFilter.getCategory(), manufacturer = itemFilter.getManufacturer();
         int rating = itemFilter.getRating(), fromPrice = itemFilter.getFromPrice(), toPrice = itemFilter.getToPrice();
 
-        boolean[] mask = new boolean[4];
+        boolean[] mask = new boolean[5];
 
         searchText = searchText.toLowerCase();
         for (Item item : originalItems) {
@@ -83,12 +86,12 @@ public class FilteredItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
                 mask[MANUFACTURER] = false;
             if (rating != 0 && item.getRating() != rating)
                 mask[RATING] = false;
-            if (fromPrice != 0 && toPrice != 0 &&
-                    !(item.getUnitPrice() >= fromPrice && item.getUnitPrice() <= toPrice)) {
-                mask[PRICE] = false;
-            }
+            if (fromPrice != 0 && item.getUnitPrice() < fromPrice)
+                mask[FROM_PRICE] = false;
+            if (toPrice != 0 && item.getUnitPrice() > toPrice)
+                mask[TO_PRICE] = false;
 
-            boolean addItem = mask[CATEGORY] & mask[MANUFACTURER] & mask[RATING] & mask[PRICE];
+            boolean addItem = mask[CATEGORY] & mask[MANUFACTURER] & mask[RATING] & mask[FROM_PRICE] & mask[TO_PRICE];
             if (addItem)
                 filteredItems.add(item);
         }
--- app/src/main/java/com/example/accessotech/adapter/ItemFilter.java
+++ app/src/main/java/com/example/accessotech/adapter/ItemFilter.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public class ItemFilter {
 
-    public static final int CATEGORY = 0, MANUFACTURER = 1, RATING = 2, PRICE = 3;
+    public static final int CATEGORY = 0, MANUFACTURER = 1, RATING = 2, FROM_PRICE = 3, TO_PRICE = 4;
 
     private String category, manufacturer;
     private int rating, fromPrice, toPrice;
