a1980c2af83b8346579f49373fbcd360d5504000
Mays Al-Reem <hroubmays@gmail.com>
2025-04-18 22:23:12 +0300

    feat: implement user profile functionality

--- app/src/main/java/com/example/accessotech/activity/UserProfileActivity.java
+++ app/src/main/java/com/example/accessotech/activity/UserProfileActivity.java
@@ -1,6 +1,10 @@
 package com.example.accessotech.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.accessotech.R;
+import com.example.accessotech.util.PrefsKeys;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -22,5 +27,29 @@ public class UserProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setUpViews();
+    }
+
+    private void setUpViews() {
+        TextView txtViewUsername = findViewById(R.id.txtViewUsername),
+                txtViewEmail = findViewById(R.id.txtViewEmail),
+                txtViewPassword = findViewById(R.id.txtViewPassword),
+                txtViewAddress = findViewById(R.id.txtViewAddress),
+                txtViewPhoneNumber = findViewById(R.id.txtViewPhoneNumber),
+                txtViewCardNumber = findViewById(R.id.txtViewCardNumber);
+
+        SharedPreferences prefs = getSharedPreferences(PrefsKeys.ACCOUNT_INFO_SHARED_PREF.getKey(), MODE_PRIVATE);
+        txtViewUsername.setText(prefs.getString(PrefsKeys.USERNAME.getKey(), "None"));
+        txtViewEmail.setText(prefs.getString(PrefsKeys.EMAIL.getKey(), "None"));
+        txtViewPassword.setText(prefs.getString(PrefsKeys.PASSWORD.getKey(), "None"));
+        txtViewAddress.setText(prefs.getString(PrefsKeys.ADDRESS.getKey(), "None"));
+        txtViewPhoneNumber.setText(prefs.getString(PrefsKeys.PHONE_NUMBER.getKey(), "None"));
+        txtViewCardNumber.setText(prefs.getString(PrefsKeys.CARD_NUMBER.getKey(), "None"));
+    }
+
+    public void logout(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
